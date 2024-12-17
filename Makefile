0d9bc7cb65e9ad99e60b974d900f4722c8aa42fe
brtvnk14 <brtvnk14@gmail.com>
2024-12-17 17:12:14 +0530

    first commit for kernel dev

---
+++ Makefile
@@ -0,0 +1,8 @@
+all:
+	# nasm -f elf32 ./protected_mode.asm -o ./protected.o
+	# nasm -f elf32 ./boot.asm -o ./boot.o
+	# ld -m elf_i386 protected.o boot.o -o boot
+	# dd if=./protected.bin >> ./boot.bin
+	nasm -f bin ./boot.asm -o ./boot.bin
+	# dd if=./hello.txt >> ./boot.bin
+	# dd if=/dev/zero bs=512 count=1 >> boot.bin
---
+++ boot.asm
@@ -0,0 +1,104 @@
+ORG 0x0
+; section .text
+BITS 16
+; extern load_protecetd
+CODE_SEG equ gdt_code-gdt_start
+DATA_SEG equ gdt_data-gdt_start
+
+__start:
+    jmp short start
+    nop
+times 33 db 0
+
+start:
+    jmp 0x7c0:start_section
+
+start_section:
+    cli ;Clear interrupts
+    mov ax,0x7c0
+    ; mov ax,0x6c0
+    mov ds,ax
+    mov es,ax
+    mov ax,0x00
+    mov ss,ax
+    mov sp,0x7c00
+    sti;Enables interrupts
+    times 510 - ($ - $$) db 0
+    dw 0xAA55
+    ; mov ah,2
+    ; mov al,1
+    ; mov ch,0
+    ; mov cl,2
+    ; mov dh,0
+    ; mov bx,buffer
+    ; int 0x13
+    ; jc error
+
+    ; mov si,buffer
+    ; call print
+    ;mov bx,test_buffer
+    ;mov si,test_buffer
+    ;call print
+    ; jmp $
+    ; call load_protecetd
+
+.load_protecetd:
+    cli
+    lgdt[gdt_desc]
+    mov eax,cr0
+    or eax,0x1
+    mov cr0,eax
+    jmp CODE_SEG:load32
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+;offset 0x8
+gdt_code: ;Code segment
+    dw 0xFFFF   ;segmnent limit first 0-15bits
+    dw 0        ;base first 0-15 bits
+    db 0        ;base 16-23 bits
+    db 0x9a     ;access byte
+    db 11001111b    ;high 4 bit flags and the low 4 bit flags
+    db 0            ; base 24-31 bits
+;offset 0x10
+gdt_data:   ;data segment,ss,es
+    dw 0xFFFF   ;segmnent limit first 0-15bits
+    dw 0        ;base first 0-15 bits
+    db 0        ;base 16-23 bits
+    db 0x92     ;access byte
+    db 11001111b    ;high 4 bit flags and the low 4 bit flags
+    db 0            ; base 24-31 bits
+gdt_end:
+gdt_desc:
+    dw gdt_end - gdt_start -1
+    dd  gdt_start
+
+BITS 32
+load32:
+    jmp $
+
+error:
+    mov si,error_msg
+    call print
+    jmp $
+print:
+    mov bx,0
+.loop:
+    lodsb
+    cmp al,0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+print_char:
+    mov ah,0eh
+    int 0x10
+    ret
+error_msg: db 'Fail to load disk',0
+message: db 'Hello world',0
+; times 510 - ($ - $$) db 0
+; dw 0xAA55
+buffer:
+;test_buffer: db 'Hellllllo',0
